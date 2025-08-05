7285174677395eccb6f99c3e39cd49b71a01d2ad
Dev Dixit <devdixitsocial@gmail.com>
2025-08-05 13:11:52 +0530

    add: login for admin, middleware for admin route

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import Client from './models/client.model.js';
 import { sendMail } from './services/mailer.service.js';
 import { EmailTemp } from './templates/email.temp.js';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
 
 await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME })
   .then(() => { console.log(`DB CONNECTED`) })
@@ -14,17 +16,44 @@ const app = express();
 const port = process.env.PORT || 4000
 
 app.use(cors({
-  origin: "https://itagfinancials.vercel.app",
+  origin: process.env.FRONTEND_PRO_URL || process.env.FRONTEND_DEV_URL,
   credentials: true
 }));
 
-
+app.use(cookieParser());
 app.use(express.json());
 app.use((req, _, next) => {
   console.log(`${req.method} - ${req.url}`);
   next();
 });
 
+// middlewares
+const AuthMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if(!token) {
+    return res.status(400).json({
+      message: 'No token provided'
+    });
+  }
+
+  const decodeJwt = jwt.verify(token, process.env.JWT_SEC);
+  const checkRole = decodeJwt.role === 'admin' || 'developer'
+
+  if(!checkRole) {
+    return res.json({
+      message: `${checkRole} is not auth`
+    })
+  }
+
+  next();
+}
+
+app.get('/', (req, res) => {
+  res.end('working');
+})
+
+// add info by clients
 app.post('/client-info', async (req, res) => {
   try {
     const newClient = await Client.create({
@@ -54,7 +83,8 @@ app.post('/client-info', async (req, res) => {
   }
 });
 
-app.get('/admin/clients', async (req, res) => {
+// admin - get clients
+app.post('/admin/clients', AuthMiddleware, async (req, res) => {
   const allClients = await Client.find();
 
   return res.status(200).json({
@@ -62,6 +92,44 @@ app.get('/admin/clients', async (req, res) => {
   });
 });
 
+// admin login
+app.post('/admin', async (req, res) => {
+  const { username, password } = req.body;
+
+  if(!username || !password) {
+    return res.status(400).json({
+      message: 'All fields are required for login'
+    });
+  }
+
+  const isAdmin = username === process.env.ADMIN_USER && password === process.env.ADMIN_PASS
+  const isDev = username === process.env.DEV_USER && password === process.env.DEV_PASS
+
+  if(!isAdmin && !isDev) {
+    return res.status(400).json({
+      message: 'Incorrect email or password'
+    });
+  }
+
+  const payload = {
+    username,
+    role: isAdmin ? 'admin' : 'developer'
+  }
+
+  const encodeJwt = jwt.sign(payload, process.env.JWT_SEC, {expiresIn: '1h'});
+
+  res.cookie('token', encodeJwt, {
+    maxAge: 60 * 60 * 1000,
+    secure: true,
+    httpOnly: true
+  });
+
+  return res.json({
+    message: `${username} logged in 🎈`,
+    role: payload.role
+  });
+});
+
 app.listen(port, () => {
-  console.log(`server is running on 0.0.0.0:${port}`);
+  console.log(`server is running on ${port}`);
 });
